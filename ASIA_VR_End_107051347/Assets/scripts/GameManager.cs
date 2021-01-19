e122ef781fe21c00fe63210980a8bf59a7364d5b
stiffgg <71679936+stiffgg@users.noreply.github.com>
2021-01-19 12:50:03 +0800

    Initial commit

---
+++ ASIA_VR_End_107051347/Assets/scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+    public class GameManager : MonoBehaviour
+    {
+        public void RestartGame()
+        {
+            SceneManager.LoadScene("大象倒地機");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+
---
+++ ASIA_VR_End_107051347/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager: MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("得分")]
+    public int scoreIn = 2;
+    [Header("進象音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "小雞雞")
+        {
+            AddScore();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "SCORE:" + score;
+        aud.PlayOneShot(soundIn, Random.Range(1f, 2f));
+    }
+}
+
+
